eecb264dd52db38f48ae27d13068dfc566f3fffb
Robert Kerr <rskerr@gmail.com>
2022-06-04 17:00:47 -0400

    Generating a random number.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@
 // A game by Robert Kerr
 
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Hello, Lets play Rock, Paper, Sissors!");
+
+    let comp_move = rand::thread_rng().gen_range(1..4);
+
+    println!("My move is {}", comp_move);
 
     println!("Please input your move.");
 
